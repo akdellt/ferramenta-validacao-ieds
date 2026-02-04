8f5441d05b0283d7a0eff629f3704c5516a98573
Danyelle M. <danyellemag123@gmail.com>
2026-02-03 23:57:33 -0300

    Update api.ts
    correção do código

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -5,9 +5,6 @@ const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 export const api = axios.create({
   baseURL: BASE_URL,
   timeout: 60000,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 api.interceptors.response.use(
